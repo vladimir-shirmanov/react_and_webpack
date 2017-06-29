c2e5a3ca90d4a8b73dc4895e0f98dd83deb0d0de
Vladimir Shirmanov <shirmanov.v@nixsolutions.com>
2017-06-29 15:13:23 +0300

    added babel loader and react to the page

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,20 @@ module.exports = {
         path: path.join(__dirname, "public"),
         filename: "bundle.js"
     },
-    
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true,
